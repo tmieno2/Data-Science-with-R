9bdc391089343e2564a6ad6fe38f6e1acb61bf00
Taro Mieno <tmieno2@unl.edu>
2022-01-27 22:03:26 -0600

    rmarkdown lecture updated

---
+++ knit_help.R
@@ -0,0 +1,6 @@
+library(here)
+
+# /*===========================================================
+#' # Chapter 2: Data Wrangling
+# /*===========================================================
+rmarkdown::render(here("Chapter-2-Rmarkdown/Rmarkdown_x.rmd"))
