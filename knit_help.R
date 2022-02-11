b11c277211000c8b5e7c6095c350a965c0212b70
Taro Mieno <tmieno2@unl.edu>
2022-02-11 11:01:00 -0600

    fix minor errors

--- knit_help.R
+++ knit_help.R
@@ -1,6 +1,11 @@
 library(here)
 
 # /*===========================================================
-#' # Chapter 2: Data Wrangling
+#' # Chapter 2: Rmarkdown
 # /*===========================================================
 rmarkdown::render(here("Chapter-2-Rmarkdown/Rmarkdown_x.rmd"))
+
+# /*===========================================================
+#' # Chapter 3: Data Wrangling
+# /*===========================================================
+rmarkdown::render(here("Chapter-3-DataWrangling/data_wrangling_x.rmd"))
